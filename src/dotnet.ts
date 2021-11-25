613953e48e7e875606425bcad2105df0255cbab9
José Simões <jose.simoes@eclo.solutions>
2021-11-25 15:51:38 +0000

    Code improvements (#4)
    ***NO_CI***

--- src/dotnet.ts
+++ src/dotnet.ts
@@ -13,19 +13,19 @@ export class Dotnet {
     /**
      * Builds the nanoFramework solution in a Terminal using MSBuild.exe (win32) or msbuild from mono (linux/macOS)
      * @param fileUri absolute path to *.sln
-     * @param nanoFrameworkExtensionPath absolute path to root of nanoFramework extension 
+     * @param toolPath absolute path to root of nanoFramework extension 
      */
-    public static build(fileUri: string, nanoFrameworkExtensionPath: String) {
+    public static build(fileUri: string, toolPath: String) {
         if (fileUri) {
             // using dynamicly-solved MSBuild.exe when ran from win32
             if(os.platform() === "win32") {
                 Executor.runInTerminal('$path = & "${env:ProgramFiles(x86)}\\microsoft visual studio\\installer\\vswhere.exe" -latest -prerelease -requires Microsoft.Component.MSBuild -find MSBuild\\**\\Bin\\MSBuild.exe | select-object -first 1; ' +
-                    nanoFrameworkExtensionPath + '/nuget/nuget.exe restore ' + fileUri + '; ' +
-                    '& $path ' + fileUri + ' -p:NanoFrameworkProjectSystemPath=' + nanoFrameworkExtensionPath + '/nanoFramework/v1.0/');
+                    toolPath + '/nuget/nuget.exe restore ' + fileUri + '; ' +
+                    '& $path ' + fileUri + ' -p:NanoFrameworkProjectSystemPath=' + toolPath + '/nanoFramework/v1.0/');
             }
             // using msbuild (comes with mono-complete) on unix 
             else {
-                Executor.runInTerminal(`nuget restore "${fileUri}" && msbuild "${fileUri}" -p:NanoFrameworkProjectSystemPath=${nanoFrameworkExtensionPath}/nanoFramework/v1.0/`);
+                Executor.runInTerminal(`nuget restore "${fileUri}" && msbuild "${fileUri}" -p:NanoFrameworkProjectSystemPath=${toolPath}/nanoFramework/v1.0/`);
             }
         }
     }
@@ -34,17 +34,17 @@ export class Dotnet {
      * First builds nanoFramework solution, then deploys this built solution to selected device
      * @param fileUri absolute path to *.sln 
      * @param serialPath path to connected nanoFramework device (e.g. COM4 or /dev/tty.usbserial*)
-     * @param nanoFrameworkExtensionPath absolute path to root of nanoFramework extension 
+     * @param toolPath absolute path to root of nanoFramework extension 
      */
-    public static deploy(fileUri: string, serialPath: string, nanoFrameworkExtensionPath: String) {
+    public static deploy(fileUri: string, serialPath: string, toolPath: String) {
         if (fileUri) {
             const outputDir = path.dirname(fileUri) + '/OutputDir/';
-            const cliBuildArguments = `/p:NanoFrameworkProjectSystemPath=${nanoFrameworkExtensionPath}/nanoFramework/v1.0/ /p:OutDir=${outputDir}`;
-            const cliDeployArguments = `${nanoFrameworkExtensionPath}/nanoFrameworkDeployer/nanoFrameworkDeployer.exe -v ${serialPath ? '-c '+ serialPath : ''} -d ${outputDir}`;
+            const cliBuildArguments = `/p:NanoFrameworkProjectSystemPath=${toolPath}/nanoFramework/v1.0/ /p:OutDir=${outputDir}`;
+            const cliDeployArguments = `${toolPath}/nanoFrameworkDeployer/nanoFrameworkDeployer.exe -v ${serialPath ? '-c '+ serialPath : ''} -d ${outputDir}`;
 
             if(os.platform() === "win32") {
                 Executor.runInTerminal('$path = & "${env:ProgramFiles(x86)}\\microsoft visual studio\\installer\\vswhere.exe" -latest -prerelease -requires Microsoft.Component.MSBuild -find MSBuild\\**\\Bin\\MSBuild.exe | select-object -first 1; ' +
-                    nanoFrameworkExtensionPath + '/nuget/nuget.exe restore ' + fileUri + '; ' +
+                    toolPath + '/nuget/nuget.exe restore ' + fileUri + '; ' +
                     '& $path ' + fileUri + ' ' + cliBuildArguments + '; '+ 
                     cliDeployArguments);            
             }
@@ -62,18 +62,18 @@ export class Dotnet {
      * @param fileUri absolute path to *.sln 
      * @param serialPath path to connected nanoFramework device (e.g. COM4 or /dev/tty.usbserial*)
      * @param targetImage the type of device connected (e.g. ESP32_REV0, ESP32_PICO)
-     * @param nanoFrameworkExtensionPath absolute path to root of nanoFramework extension 
+     * @param toolPath absolute path to root of nanoFramework extension 
      */
-    public static deployAlternative(fileUri: string, serialPath: string, targetImage: string, nanoFrameworkExtensionPath: String) {
+    public static deployAlternative(fileUri: string, serialPath: string, targetImage: string, toolPath: String) {
         if (fileUri && targetImage) {
             const outputDir = path.dirname(fileUri) + '/OutputDir/';
-            const cliBuildArguments = `/p:NanoFrameworkProjectSystemPath=${nanoFrameworkExtensionPath}/nanoFramework/v1.0/ /p:OutDir=${outputDir}`;
-            const cliBuildBin = `${nanoFrameworkExtensionPath}/nanoFrameworkDeployer/nanoFrameworkDeployer.exe -v -d ${outputDir} -b`;
-            const cliDeploy = `dotnet ${nanoFrameworkExtensionPath}/nanoFirmwareFlasher/nanoff.dll --target ${targetImage} --serialport ${serialPath} --deploy --image ${outputDir}deploy.bin`;
+            const cliBuildArguments = `/p:NanoFrameworkProjectSystemPath=${toolPath}/nanoFramework/v1.0/ /p:OutDir=${outputDir}`;
+            const cliBuildBin = `${toolPath}/nanoFrameworkDeployer/nanoFrameworkDeployer.exe -v -d ${outputDir} -b`;
+            const cliDeploy = `dotnet ${toolPath}/nanoFirmwareFlasher/nanoff.dll --target ${targetImage} --serialport ${serialPath} --deploy --image ${outputDir}deploy.bin`;
 
             if(os.platform() === "win32") {
                 Executor.runInTerminal('$path = & "${env:ProgramFiles(x86)}\\microsoft visual studio\\installer\\vswhere.exe" -latest -prerelease -requires Microsoft.Component.MSBuild -find MSBuild\\**\\Bin\\MSBuild.exe | select-object -first 1; ' +
-                    nanoFrameworkExtensionPath + '/nuget/nuget.exe restore ' + fileUri + '; ' +
+                    toolPath + '/nuget/nuget.exe restore ' + fileUri + '; ' +
                     '& $path ' + fileUri + ' ' + cliBuildArguments + '; '+ 
                     cliBuildBin + '; ' +
                     cliDeploy);            
@@ -89,12 +89,12 @@ export class Dotnet {
 
     /**
      * Flashes the selected device to new firmware using nanoFirmwareFlasher
-     * @param nanoFrameworkExtensionPath absolute path to root of nanoFramework extension 
+     * @param toolPath absolute path to root of nanoFramework extension 
      * @param cliArguments CLI arguments passed to nanoff.dll
      */
-    public static flash(nanoFrameworkExtensionPath: String, cliArguments: String) {
-        if(nanoFrameworkExtensionPath && cliArguments) {
-            Executor.runInTerminal(`dotnet ${nanoFrameworkExtensionPath}/nanoFirmwareFlasher/nanoff.dll --update ${cliArguments}`);
+    public static flash(toolPath: String, cliArguments: String) {
+        if(toolPath && cliArguments) {
+            Executor.runInTerminal(`dotnet ${toolPath}/nanoFirmwareFlasher/nanoff.dll --update ${cliArguments}`);
         }
     }
 }
