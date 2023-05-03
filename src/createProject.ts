ea2891cbafcb6c14af8d71780e8a2e205c0af14a
Laurent Ellerbach <laurelle@microsoft.com>
2023-05-03 19:16:29 +0200

    Creating blank application

--- src/createProject.ts
+++ src/createProject.ts
@@ -9,6 +9,7 @@ import * as os from 'os';
 
 import { Executor } from "./executor";
 import { Console, debug } from "console";
+import * as crypto from "crypto";
 
 export class NfProject {
     /**
@@ -23,22 +24,111 @@ export class NfProject {
     public static async AddProject(fileUri: string, projectName: string, projectType: string, toolPath: String) {
         //TODO : Add the project
         var fs = require('fs');
-        const nodePath = require('path');
+        const path = require('path');
+        var solutionPath = path.dirname(fileUri);
         switch (projectType) {
             default:
             case "Blank Application":
                 // First open the nfproj template file
-                var filePath = nodePath.join(toolPath, 'CS.BlankApplication-vs2022', 'CS.BlankApplication-vs2022.vstemplate');
+                var filePath = path.join(toolPath, 'CS.BlankApplication-vs2022', 'NFApp.nfproj');
                 fs.readFile(filePath, 'utf8', function (err: any, data: any) {
                     if (err) {
                         return console.log(err);
                     }
-        
+
                     // Replace the tokens
-                    var result = data.replace(/string to be replaced/g, 'replacement');
-        
-                    fs.writeFile(fileUri, result, 'utf8', function (err: any) {
-                        if (err) return console.log(err);
+                    // First one is the project name $safeprojectname$
+                    var result = data.replace(/\$safeprojectname\$/g, projectName);
+
+                    // Second one is the project guid $guid1$
+                    let id = crypto.randomUUID();
+                    result = result.replace(/\$guid1\$/g, id);
+
+                    var filePath = path.join(solutionPath, projectName, projectName + '.nfproj');
+                    fs.mkdir(path.dirname(filePath), { recursive: true }, (err: any) => {
+                        if (err) {
+                            return console.log(err);
+                        }
+
+                    });
+
+                    fs.writeFile(filePath, result, 'utf8', function (err: any) {
+                        if (err) {
+                            return console.log(err);
+                        }
+
+                        // Second open the Program.cs file
+                        var filePath = path.join(toolPath, 'CS.BlankApplication-vs2022', 'Program.cs');
+                        fs.readFile(filePath, 'utf8', function (err: any, data: any) {
+                            if (err) {
+                                return console.log(err);
+                            }
+
+                            // Replace the tokens
+                            // First one is the project name $safeprojectname$
+                            var result = data.replace(/\$safeprojectname\$/g, projectName);
+                            var filePath = path.join(solutionPath, projectName, 'Program.cs');
+                            fs.mkdir(path.dirname(filePath), { recursive: true }, (err: any) => {
+                                if (err) {
+                                    return console.log(err);
+                                }
+
+                            });
+
+                            fs.writeFile(filePath, result, 'utf8', function (err: any) {
+                                if (err) {
+                                    return console.log(err);
+                                }
+
+                                // Finally remove the year and organization from the AssemblyInfo.cs file
+                                var filePath = path.join(toolPath, 'CS.BlankApplication-vs2022', 'AssemblyInfo.cs');
+                                fs.readFile(filePath, 'utf8', function (err: any, data: any) {
+                                    if (err) {
+                                        return console.log(err);
+                                    }
+
+                                    // Replace the tokens
+                                    // Remove both $registeredorganization$ $year$
+                                    var result = data.replace(/\$registeredorganization\$/g, '');
+                                    result = result.replace(/\$year\$/g, '');
+
+                                    var filePath = path.join(solutionPath, projectName, 'Properties', 'AssemblyInfo.cs');
+                                    fs.mkdir(path.dirname(filePath), { recursive: true }, (err: any) => {
+                                        if (err) {
+                                            return console.log(err);
+                                        }
+
+                                    });
+
+                                    fs.writeFile(filePath, result, 'utf8', function (err: any) {
+                                        if (err) {
+                                            return console.log(err);
+                                        }
+
+                                        // Finally add the project to the solution
+                                        Executor.runInTerminal("dotnet sln " + fileUri + " add " + path.join(solutionPath, projectName, projectName + '.nfproj'));
+                                        // Wait for a second to have the command executed
+                                        setTimeout(() => {
+                                            // And open the sln project, replace the GUID of the added project with the one in the nfproj file
+                                            fs.readFile(fileUri, 'utf8', function (err: any, data: any) {
+                                                if (err) {
+                                                    return console.log(err);
+                                                }
+
+                                                // Replace the guid by te nanoframework one
+                                                let stringToReplace = RegExp('Project\(\"{(.*)}\"\) = \"' + projectName, 'g');
+                                                var result = data.replace(stringToReplace, '11A8DD76-328B-46DF-9F39-F559912D0360');
+                                                fs.writeFile(fileUri, result, 'utf8', function (err: any) {
+                                                    if (err) {
+                                                        return console.log(err);
+                                                    }
+                                                });
+                                            });
+                                        }, 3000);
+                                    });
+                                });
+                            });
+                        });
                     });
                 });
 
